082d9a81b9a64af236af06c3421871b17f5cbe31
Xumoyun <rabbimovxumoyun22@gmail.com>
2025-08-19 08:08:07 +0500

    firt changes are added

---
+++ client/app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import { Roboto } from "next/font/google";
+import "./globals.css";
+
+const roboto = Roboto({
+  variable: "--font-roboto",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Telegram Clone",
+  description:
+    "Telegram is a cloud-based mobile and desktop messaging app with a focus on security and speed.",
+  icons: {
+    icon: "./logo.svg",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${roboto.variable} antialiased`}>{children}</body>
+    </html>
+  );
+}
