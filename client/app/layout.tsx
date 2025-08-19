628fd6042be5b73cf395548ac626b7b7b9994718
Xumoyun <rabbimovxumoyun22@gmail.com>
2025-08-19 22:44:56 +0500

    2. second layout changes

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { ThemeProvider } from "./_providers/theme-provider";
 
 const roboto = Roboto({
+  weight: ["300", "400", "500", "600", "700"],
   variable: "--font-roboto",
   subsets: ["latin"],
 });
